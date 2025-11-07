425399c6fb765f26790bd300e252d198031e5506
AJoelCastro <misthorn33@gmail.com>
2025-11-07 13:00:11 -0500

    IV controller and service 2

--- src/main/java/com/arturocastro/apiimagesvisionservice/controller/ImagesVisionController.java
+++ src/main/java/com/arturocastro/apiimagesvisionservice/controller/ImagesVisionController.java
@@ -1,8 +1,25 @@
 package com.arturocastro.apiimagesvisionservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.arturocastro.apiimagesvisionservice.model.IVModel;
+import com.arturocastro.apiimagesvisionservice.service.ImagesVisionSercive;
+import com.openai.models.images.ImagesResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@RequestMapping("/api/openai/images-vision")
 public class ImagesVisionController {
 
+    private final ImagesVisionSercive imagesVisionSercive;
+
+    public ImagesVisionController(ImagesVisionSercive imagesVisionSercive) {
+        this.imagesVisionSercive = imagesVisionSercive;
+    }
+
+    @PostMapping("/generate-image")
+    public ResponseEntity<ImagesResponse> generateImage(@RequestBody IVModel ivModel) throws Exception{
+        ImagesResponse imagesResponse = imagesVisionSercive.generateImage(ivModel);
+        return ResponseEntity.ok(imagesResponse);
+    }
 }
--- src/main/java/com/arturocastro/apiimagesvisionservice/service/ImagesVisionSercive.java
+++ src/main/java/com/arturocastro/apiimagesvisionservice/service/ImagesVisionSercive.java
@@ -1,8 +1,35 @@
 package com.arturocastro.apiimagesvisionservice.service;
 
+import com.arturocastro.apiimagesvisionservice.model.IVModel;
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.images.ImageGenerateParams;
+import com.openai.models.images.ImageModel;
+import com.openai.models.images.ImagesResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImagesVisionSercive {
+
+    private final OpenAIClient client;
+
+    public ImagesVisionSercive(@Value("openai.api.key") String apiKey) {
+        client = new OpenAIOkHttpClient.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
+    public ImagesResponse generateImage (IVModel ivModel){
+
+        ImageGenerateParams imageGenerateParams = ImageGenerateParams.builder()
+                .model(ImageModel.GPT_IMAGE_1_MINI)
+                .n(1)
+                .prompt(ivModel.getPromtp())
+                .size(ImageGenerateParams.Size._256X256)
+                .build();
+
+        return client.images().generate(imageGenerateParams);
+    }
 
 }
