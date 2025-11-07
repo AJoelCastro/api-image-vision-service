70c72c443ebb3f1fe89de35571411914349b9bc3
AJoelCastro <misthorn33@gmail.com>
2025-11-07 12:21:31 -0500

    first commit

---
+++ src/main/java/com/arturocastro/apiimagesvisionservice/ApiImagesVisionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.arturocastro.apiimagesvisionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiImagesVisionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiImagesVisionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arturocastro/apiimagesvisionservice/ApiImagesVisionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturocastro.apiimagesvisionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiImagesVisionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
