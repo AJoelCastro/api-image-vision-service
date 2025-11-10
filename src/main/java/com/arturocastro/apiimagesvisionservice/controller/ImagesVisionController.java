945ba1550f96aa5c599b15ceb169274c6547c607
AJoelCastro <misthorn33@gmail.com>
2025-11-09 23:19:34 -0500

    analyze falta mejorar

--- src/main/java/com/arturocastro/apiimagesvisionservice/controller/ImagesVisionController.java
+++ src/main/java/com/arturocastro/apiimagesvisionservice/controller/ImagesVisionController.java
@@ -3,6 +3,7 @@ package com.arturocastro.apiimagesvisionservice.controller;
 import com.arturocastro.apiimagesvisionservice.model.IVModel;
 import com.arturocastro.apiimagesvisionservice.service.ImagesVisionSercive;
 import com.openai.models.images.ImagesResponse;
+import com.openai.models.responses.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,4 +24,11 @@ public class ImagesVisionController {
         ImagesResponse imagesResponse = imagesVisionSercive.generateImage(prompt);
         return ResponseEntity.ok(imagesResponse);
     }
+
+    @PostMapping("analyze-image")
+    public ResponseEntity<Response> analyzeImage(@RequestBody String uri) throws Exception{
+        Response response = imagesVisionSercive.analyzeImage(uri);
+        return ResponseEntity.ok(response);
+    }
+
 }
--- src/main/java/com/arturocastro/apiimagesvisionservice/service/ImagesVisionSercive.java
+++ src/main/java/com/arturocastro/apiimagesvisionservice/service/ImagesVisionSercive.java
@@ -3,9 +3,15 @@ package com.arturocastro.apiimagesvisionservice.service;
 import com.arturocastro.apiimagesvisionservice.model.IVModel;
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.ChatModel;
+import com.openai.models.graders.gradermodels.LabelModelGrader;
 import com.openai.models.images.ImageGenerateParams;
 import com.openai.models.images.ImageModel;
 import com.openai.models.images.ImagesResponse;
+import com.openai.models.responses.Response;
+import com.openai.models.responses.ResponseCreateParams;
+import com.openai.models.responses.ResponseInputItem;
+import com.openai.models.responses.ResponseOutputItem;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +36,19 @@ public class ImagesVisionSercive {
                 .build();
 
         return client.images().generate(imageGenerateParams);
+    }
+
+    public Response analyzeImage(String uri){
+        ResponseCreateParams responseCreateParams = ResponseCreateParams.builder()
+                .model(ChatModel.GPT_4_1_MINI)
+                .input(
+                        ResponseCreateParams.Input
+                                .ofText("Dime que personajes ves en la imagen, describelos y sus nombres, basate en la imagen unicamente:" + uri)
+                )
+                .build();
+
+        return client.responses().create(responseCreateParams);
+
     }
 
 }
