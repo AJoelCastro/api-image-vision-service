c39f47d071a7439c6953e46a4b350ff9ab29be25
AJoelCastro <misthorn33@gmail.com>
2025-11-11 14:27:08 -0500

    Image analysis request

--- src/main/java/com/arturocastro/apiimagesvisionservice/DTO/ImageAnalysisRequest.java
+++ src/main/java/com/arturocastro/apiimagesvisionservice/DTO/ImageAnalysisRequest.java
@@ -9,6 +9,6 @@ public class ImageAnalysisRequest {
     @NotBlank(message = "La URL de la imagen es obligatoria")
     private String imageUrl;
 
-    private String prompt = "¿Qué ves en esta imagen?";
+    private String prompt = "Describe lo que ves en esta imagen.";
 
 }
